cd678ee0c41e2f0ff08fcd7c7fe742c14d02cfcc
aviupadhyayula <aupadhy@gmail.com>
2022-08-22 23:26:08 -0700

    Use PEGASUS for paraphrase generation

--- paraphrase_text.py
+++ paraphrase_text.py
@@ -1,54 +1,18 @@
-from parrot import Parrot
-import warnings
-import torch
-from nltk import tokenize
-import string
+from transformers import *
+import sys
 
-warnings.filterwarnings("ignore")
-parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5")
+logging.set_verbosity(logging.FATAL)
+model = PegasusForConditionalGeneration.from_pretrained("tuner007/pegasus_paraphrase")
+tokenizer = PegasusTokenizerFast.from_pretrained("tuner007/pegasus_paraphrase")
 
-def set_random_state(seed):
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
+def get_paraphrased_sentences(model, tokenizer, sentence, num_return_sequences=5, num_beams=5):
+  inputs = tokenizer([sentence], truncation=True, padding="longest", return_tensors="pt")
+  outputs = model.generate(**inputs, num_beams=num_beams, num_return_sequences=num_return_sequences)
+  return tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
-def paraphrase(text):
-    has_offer = False
-    if "$session.params" in text:
-        has_offer = True
-        text = text.replace("$$session.params.counter_offer", "my counter offer")
-    sentences = tokenize.sent_tokenize(text)
-    t_paraphrases = []
-    max = 0
-    for sentence in sentences:
-        paras_diverse = parrot.augment(input_phrase=sentence, do_diverse=True)
-        paras_undiverse = parrot.augment(input_phrase=sentence, do_diverse=False)
-        if paras_diverse and not paras_undiverse:
-            paras = paras_diverse
-        elif paras_undiverse and not paras_diverse:
-            paras = paras_undiverse
-        elif not paras_undiverse and not paras_diverse:
-            paras = [(sentence, 0)]
-        else:
-            if len(paras_diverse) <= len(paras_undiverse):
-                paras = paras_diverse 
-            else: 
-                paras = paras_undiverse
-        paras = [para[0] for para in paras]
-        if len(paras) > max:
-            max = len(paras)
-        t_paraphrases.append(paras)
-    results = []
-    for i in range(max):
-        sentence = ""
-        for j in range(len(t_paraphrases)):
-            try:
-                sentence += t_paraphrases[j][i] + ". "
-            except:
-                pass
-        if has_offer:
-            sentence = sentence.replace("my counter offer", "$$session.params.counter_offer")
-            if "$session.params.counter_offer" not in sentence:
-                sentence += "How about $$session.params.counter_offer?" + ". "
-        results.append(sentence[: len(sentence) - 2])
-    return results
+def paraphrase_sentence(sentence):
+    return get_paraphrased_sentences(model, tokenizer, sentence, num_beams=10, num_return_sequences=5)
+
+def main():
+    print(paraphrase_sentence(sys.argv[1]))
+    sys.stdout.flush()
